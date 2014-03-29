934022e85fcc5771c6187fefe5fa3fba111973f5
Dima Ryskin <dima.ryskin@gmail.com>
2014-03-29 15:07:01 +0300

    All classes cleaned up

--- MainForm.cs
+++ MainForm.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace FileManager
 {
-	public partial class MainForm : Form
-	{
-	    
-		public MainForm()
-		{
-			InitializeComponent();
-		}
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
 
         public SidePanel SidePanelLeft
         {
@@ -29,6 +18,5 @@ namespace FileManager
         {
             get { return sidePanel2; }
         }
-
-	}
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FileManager
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
             //Operation.Subscribe();
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			//Application.Run(new MainForm());
-		    var app = new Controller();
-
-		}
-	}
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new MainForm());
+            var app = new Controller();
+        }
+    }
 }
